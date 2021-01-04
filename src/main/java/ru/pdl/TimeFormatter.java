add6bb0076680d867df183658238c72a6d11905b
Dmitriy Pronichev <dm.proni@gmail.com>
2021-01-04 13:29:55 +0300

    time formatter done

---
+++ src/main/java/ru/pdl/TimeFormatter.java
@@ -0,0 +1,66 @@
+package ru.pdl;
+
+public class TimeFormatter {
+  public static final int NUM_OF_SECONDS_IN_ONE_MINUTE = 60;
+  public static final int NUM_OF_SECONDS_IN_ONE_HOUR = NUM_OF_SECONDS_IN_ONE_MINUTE * 60;
+  public static final int NUM_OF_SECONDS_IN_ONE_DAY = NUM_OF_SECONDS_IN_ONE_HOUR * 24;
+  public static final int NUM_OF_SECONDS_IN_A_YEAR = NUM_OF_SECONDS_IN_ONE_DAY * 365;
+  public static final String NOW_LITERAL = "now";
+  public static final int[] TIME_PROGRESSION_FACTORS = new int[]{
+    NUM_OF_SECONDS_IN_A_YEAR, NUM_OF_SECONDS_IN_ONE_DAY, NUM_OF_SECONDS_IN_ONE_HOUR, NUM_OF_SECONDS_IN_ONE_MINUTE, 1};
+  public static final String[] TIME_NAMES = new String[]{"year", "day", "hour", "minute", "second"};
+
+  public static String formatDuration(int seconds) {
+    if (seconds < 0) {
+      throw new UnsupportedOperationException();
+    } else if (seconds == 0) {
+      return NOW_LITERAL;
+    }
+
+    final StringBuilder result = new StringBuilder();
+
+    final int[] timeArray = fillTimeArray(seconds);
+    for (int i = 0; i < timeArray.length; i++) {
+      addTimeName(result, timeArray, i);
+    }
+    return result.toString();
+  }
+
+  private static int[] fillTimeArray(final int seconds) {
+    final int[] timeArray = new int[5];
+
+    int secondsRest = seconds;
+
+    for (int i = 0; i < TIME_PROGRESSION_FACTORS.length; i++) {
+      timeArray[i] = secondsRest / TIME_PROGRESSION_FACTORS[i];
+      secondsRest %= TIME_PROGRESSION_FACTORS[i];
+    }
+
+    return timeArray;
+  }
+
+  private static void addTimeName(final StringBuilder result, final int[] timeArray, final int index) {
+    final int count = timeArray[index];
+    if (count > 0) {
+      if (!result.toString().isEmpty()) {
+        result.append(countPositiveAfterIndex(timeArray, index + 1) > 0 ? ", " : " and ");
+      }
+
+      result.append(count);
+      result.append(' ');
+      result.append(TIME_NAMES[index]);
+      if (count > 1) {
+        result.append('s');
+      }
+    }
+  }
+
+  private static int countPositiveAfterIndex(final int[] array, final int startIndex) {
+    int result = 0;
+    for (int i = startIndex; i < array.length; i++) {
+      result += array[i] > 0 ? 1 : 0;
+    }
+    return result;
+  }
+
+}
---
+++ src/test/java/ru/pdl/TimeFormatterTest.java
@@ -0,0 +1,79 @@
+package ru.pdl;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+class TimeFormatterTest {
+
+  @Test
+  void givenNegativeSeconds_ShouldReturnNull() {
+    Assertions.assertThrows(UnsupportedOperationException.class, () -> TimeFormatter.formatDuration(-5));
+  }
+
+  @Test
+  void givenZeroSeconds_ShouldReturnNow() {
+    Assertions.assertEquals("now", TimeFormatter.formatDuration(0));
+  }
+
+  @Test
+  void givenOnlySecond_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("1 second", TimeFormatter.formatDuration(1));
+  }
+
+  @Test
+  void givenOnlySeconds_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("2 seconds", TimeFormatter.formatDuration(2));
+  }
+
+  @Test
+  void givenOneMinute_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("1 minute", TimeFormatter.formatDuration(60));
+  }
+
+  @Test
+  void givenFewMinutes_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("3 minutes", TimeFormatter.formatDuration(60 * 3));
+  }
+
+  @Test
+  void givenFewMinutesAndSeconds_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("3 minutes and 15 seconds", TimeFormatter.formatDuration(60 * 3 + 15));
+  }
+
+  @Test
+  void givenAnHour_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("1 hour", TimeFormatter.formatDuration(60 * 60));
+  }
+
+  @Test
+  void givenFewHours_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("3 hours", TimeFormatter.formatDuration(60 * 60 * 3));
+  }
+
+  @Test
+  void givenFewHoursAndMinutesAndSeconds_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("3 hours, 2 minutes and 25 seconds", TimeFormatter.formatDuration(60 * 60 * 3 + 2 * 60 + 25));
+  }
+
+  @Test
+  void givenADay_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("1 day", TimeFormatter.formatDuration(60 * 60 * 24));
+  }
+
+  @Test
+  void givenFewDaysAndSeconds_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("2 days, 1 hour, 3 minutes and 25 seconds",
+      TimeFormatter.formatDuration(60 * 60 * 24 * 2 + 60 * 60 + 60 * 3 + 25));
+  }
+
+  @Test
+  void givenAYear_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("1 year", TimeFormatter.formatDuration(60 * 60 * 24 * 365));
+  }
+
+  @Test
+  void givenFewYearsAndSeconds_ShouldReturnCorrectResult() {
+    Assertions.assertEquals("2 years and 31 seconds", TimeFormatter.formatDuration(60 * 60 * 24 * 365 * 2 + 31));
+  }
+
+}
