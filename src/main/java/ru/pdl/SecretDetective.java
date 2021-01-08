14a839a32308a5de580aa0cac8c3c2dd6c29a6b3
Dmitriy Pronichev <dm.proni@gmail.com>
2021-01-08 11:55:31 +0300

    secret detective done

---
+++ src/main/java/ru/pdl/SecretDetective.java
@@ -0,0 +1,49 @@
+package ru.pdl;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+public class SecretDetective {
+
+  private static Set<Character> getDistinctLetters(final char[][] triplets) {
+    final Set<Character> result = new HashSet<>();
+    for (char[] triplet : triplets) {
+      for (char c : triplet) {
+        result.add(c);
+      }
+    }
+    return result;
+  }
+
+  public static boolean sortIfNeeded(final List<Character> characters, final char first, final char second) {
+    final int firstIndex = characters.indexOf(first);
+    final int secondIndex = characters.indexOf(second);
+    if (firstIndex > secondIndex) {
+      characters.set(firstIndex, second);
+      characters.set(secondIndex, first);
+      return true;
+    }
+    return false;
+  }
+
+  public String recoverSecret(char[][] triplets) {
+    final List<Character> distinctLetters = new ArrayList<>(getDistinctLetters(triplets));
+    boolean atLeastOnePermutationOccurred = true;
+    while (atLeastOnePermutationOccurred) {
+      atLeastOnePermutationOccurred = false;
+      for (char[] triplet : triplets) {
+        for (int i = 0; i < triplet.length - 1; i++) {
+          if (sortIfNeeded(distinctLetters, triplet[i], triplet[i + 1])) {
+            atLeastOnePermutationOccurred = true;
+          }
+        }
+      }
+    }
+
+    return distinctLetters.stream().map(String::valueOf).collect(Collectors.joining());
+  }
+
+}
---
+++ src/test/java/ru/pdl/SecretDetectiveTest.java
@@ -0,0 +1,32 @@
+package ru.pdl;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+class SecretDetectiveTest {
+  private SecretDetective detective;
+
+  @BeforeEach
+  public void setup() {
+    detective = new SecretDetective();
+  }
+
+  @Test
+  public void secret1() {
+    char[][] triplets = {
+      {'t', 'u', 'p'},
+      {'w', 'h', 'i'},
+      {'t', 's', 'u'},
+      {'a', 't', 's'},
+      {'h', 'a', 'p'},
+      {'t', 'i', 's'},
+      {'w', 'h', 's'}
+    };
+    assertEquals("whatisup", detective.recoverSecret(triplets));
+  }
+
+
+}
